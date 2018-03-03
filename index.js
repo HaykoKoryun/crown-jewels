03e168ef739156dfff7e491a74a3f40b41f6beac
Hayko Koryun <haykokoryun@gmail.com>
2018-03-03 21:25:25 +0400

    colorised super important header

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+const chalk = require('chalk');
+
 const header = `
-         -̆̆̆+-
-      (͠ ͡ ^̆̆̆̆̆̆O^͡ ͠ )
-       \\~~║~~/
-       @@@@@@@
+         ${chalk.yellow('-̆̆̆+-')}
+      ${chalk.yellow('(͠ ͡ ^̆̆̆̆̆̆O^͡ ͠ )')}
+       ${chalk.yellow('\\')+chalk.magenta('##')+chalk.yellow('║')+chalk.magenta('##')+chalk.yellow('/')}
+       ${chalk.yellow('[')+chalk.green('@')+chalk.yellow('-')+chalk.red('@')+chalk.yellow('-')+chalk.green('@')+chalk.yellow(']')}
        -------
- ~{---------------}~`;
+ ${chalk.yellow('~{')}${chalk.red(new Array(16).join('-'))}${chalk.yellow('}~')}`;
 
 console.log(header);
